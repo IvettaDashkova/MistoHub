ce70e9239afcc5d4a93c7c5ce5d9089599f62ac9
IvettaDashkova <ivettafsd@gmail.com>
2024-04-20 22:37:05 +0200

    add dev branch

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import VirtualTour from '../../components/VirtualTour/VirtualTour';
 const HomePage = () => {
   return (
     <Container>
+      <h1>Home Page</h1>
       <VirtualTour/>
     </Container>
   );
