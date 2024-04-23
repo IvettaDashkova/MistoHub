e033b7445fbf3e56e5814b584573fa30079f5035
IvettaDashkova <ivettafsd@gmail.com>
2024-04-23 11:54:51 +0200

    add services

---
+++ src/services/API.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+
+export async function fetchGoal() {
+  try {
+    const {
+      data: { result },
+    } = await axios.get(
+      `https://${import.meta.env.VITE_ADMIN_PROJECT_ID}.api.sanity.io/${
+        import.meta.env.VITE_ADMIN_API_VERSION
+      }/data/query/${
+        import.meta.env.VITE_ADMIN_DATASET
+      }?query=*[_type=="goal"]{ _id, goal, raised }`
+    );
+    console.log('goal', result[0]);
+    return result[0];
+  } catch (error) {
+    //    handle error by some notification
+    return;
+  }
+}
+export async function fetchCompanies() {
+  try {
+    const {
+      data: { result },
+    } = await axios.get(
+      `https://${import.meta.env.VITE_ADMIN_PROJECT_ID}.api.sanity.io/${
+        import.meta.env.VITE_ADMIN_API_VERSION
+      }/data/query/${
+        import.meta.env.VITE_ADMIN_DATASET
+      }?query=*[_type=="companies"]{ _id, id, name, logoURL, link, question, answer } | order(id asc)`
+    );
+    console.log('companies', result);
+    return result;
+  } catch (error) {
+    //    handle error by some notification
+    return;
+  }
+}
+export async function fetchPeople() {
+  try {
+    const {
+      data: { result },
+    } = await axios.get(
+      `https://${import.meta.env.VITE_ADMIN_PROJECT_ID}.api.sanity.io/${
+        import.meta.env.VITE_ADMIN_API_VERSION
+      }/data/query/${
+        import.meta.env.VITE_ADMIN_DATASET
+      }?query=*[_type=="people"]{ _id, id,firstName,secondName,type, imageURL, facebook, instagram, otherLink, question, answer  } | order(id asc)`
+    );
+    console.log('people', result);
+    return result;
+  } catch (error) {
+    //    handle error by some notification
+    return;
+  }
+}
