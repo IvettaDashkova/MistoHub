272917580a88ea37eb2914b54064a1089156f120
IvettaDashkova <ivettafsd@gmail.com>
2024-04-23 12:04:22 +0200

    add for test deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; 
 import GlobalStyle from './globalStyles';
-
+//test
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/MistoHub">
