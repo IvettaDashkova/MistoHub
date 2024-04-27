3a6bcac47ca5469c1e06c66f884e40bec9150cfe
Yuriy2223 <yuriy.shukan@gmail.com>
2024-04-27 23:47:53 +0300

    1

--- src/components/Visualization/Icon.jsx
+++ src/components/Visualization/Icon.jsx
@@ -1,20 +1,9 @@
-// import sprite from '../../assets/icons.svg';
-
-// const Icon = ({ width, height, iconName, fill, stroke }) => {
-//   return (
-//     <svg width={width} height={height} fill={fill} stroke={stroke}>
-//       <use href={`${sprite}#${iconName}`}></use>
-//     </svg>
-//   );
-// };
-
-// export default Icon;
 import sprite from '../../assets/icons.svg';
 import styled from 'styled-components';
 
 const StyledIcon = styled.svg`
-  fill: ${props => props.fill};
-  stroke: ${props => props.stroke};
+  fill: ${(props) => props.fill};
+  stroke: ${(props) => props.stroke};
 `;
 
 const Icon = ({ width, height, iconName, fill, stroke }) => {
