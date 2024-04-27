06b5f97112c540dd1472b9dd131da75a34951c00
demchukv <demchukv@gmail.com>
2024-04-27 08:51:35 +0300

    About 1.1

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -41,11 +41,11 @@ const HomePage = () => {
   console.log('done');
   return (
     <Container>
-      {/* <Icon width="40" height="30" iconName="icon-building1" styles="vector-svg" />
+      <Icon width="40" height="30" iconName="icon-building1" styles="vector-svg" />
 
-      <h1>Home Page</h1> */}
+      <h1>Home Page</h1>
       <About />
-      {/* {goalData && (
+      {goalData && (
         <>
           <p>{goalData.raised}</p> <p>{goalData.goal}</p>
         </>
@@ -53,7 +53,7 @@ const HomePage = () => {
       {companiesData && <img src={formImgURL(companiesData[0].logoURL)} />}
       {peopleData && <img src={formImgURL(peopleData[0].imageURL)} />}
       <VirtualTour />
-      <ZibraniKoshti /> */}
+      <ZibraniKoshti />
     </Container>
   );
 };
