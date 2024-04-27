71ecdfca1a09d3062bad3e4c71655b7cda7ddf78
Yuriy2223 <yuriy.shukan@gmail.com>
2024-04-27 23:40:30 +0300

    svg

---
+++ src/components/Visualization/Icon.jsx
@@ -0,0 +1,28 @@
+// import sprite from '../../assets/icons.svg';
+
+// const Icon = ({ width, height, iconName, fill, stroke }) => {
+//   return (
+//     <svg width={width} height={height} fill={fill} stroke={stroke}>
+//       <use href={`${sprite}#${iconName}`}></use>
+//     </svg>
+//   );
+// };
+
+// export default Icon;
+import sprite from '../../assets/icons.svg';
+import styled from 'styled-components';
+
+const StyledIcon = styled.svg`
+  fill: ${props => props.fill};
+  stroke: ${props => props.stroke};
+`;
+
+const Icon = ({ width, height, iconName, fill, stroke }) => {
+  return (
+    <StyledIcon width={width} height={height} fill={fill} stroke={stroke}>
+      <use href={`${sprite}#${iconName}`}></use>
+    </StyledIcon>
+  );
+};
+
+export default Icon;
--- src/components/Visualization/TourModal.jsx
+++ src/components/Visualization/TourModal.jsx
@@ -1,6 +1,7 @@
 import { forwardRef, useEffect, useCallback, useRef } from 'react';
 import { ReactPhotoSphereViewer } from 'react-photo-sphere-viewer';
 import styled from 'styled-components';
+import Icon from './Icon';
 
 const ModalWrapper = styled.div`
   position: relative;
@@ -51,7 +52,15 @@ const TourModal = forwardRef(({ image, onClose }, ref) => {
         defaultZoomLvl={10}
         navbar={true}
       />
-      <CloseButton onClick={onClose}>X</CloseButton>
+      <CloseButton onClick={onClose}>
+        <Icon
+          width="18"
+          height="18"
+          iconName="icon-closeX"
+          // fill="#ffffff"
+          stroke="#ffffff"
+        />
+      </CloseButton>
     </ModalWrapper>
   );
 });
@@ -59,3 +68,81 @@ const TourModal = forwardRef(({ image, onClose }, ref) => {
 TourModal.displayName = 'TourModal';
 
 export default TourModal;
+
+/**============================================= */
+
+// import { forwardRef, useEffect, useCallback, useRef } from 'react';
+// import { ReactPhotoSphereViewer } from 'react-photo-sphere-viewer';
+// import styled from 'styled-components';
+// import Icon from './Icon';
+
+// const ModalWrapper = styled.div`
+//   position: relative;
+//   width: 100%;
+//   height: 100%;
+// `;
+
+// const CloseButton = styled.button`
+//   position: absolute;
+//   top: 10px;
+//   right: 10px;
+//   background-color: transparent;
+//   border: none;
+//   border-radius: 50%;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   cursor: pointer;
+// `;
+
+// const TourModal = forwardRef(({ image, onClose }, ref) => {
+//   const wrapperRef = useRef();
+
+//   const closeTourOnClickOutside = useCallback(
+//     (event) => {
+//       if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+//         onClose();
+//       }
+//     },
+//     [onClose]
+//   );
+
+//   useEffect(() => {
+//     document.addEventListener('mousedown', closeTourOnClickOutside);
+//     return () => {
+//       document.removeEventListener('mousedown', closeTourOnClickOutside);
+//     };
+//   }, [closeTourOnClickOutside]);
+
+//   const iconStyles = {
+//     fill: 'red',
+//     stroke: '#ffffff',
+//   };
+
+//   return (
+//     <ModalWrapper ref={wrapperRef}>
+//       <ReactPhotoSphereViewer
+//         ref={ref}
+//         src={image}
+//         hideNavbarButton={false}
+//         height={'100%'}
+//         width={'100%'}
+//         defaultZoomLvl={10}
+//         navbar={true}
+//       />
+//       <CloseButton onClick={onClose}>
+//         X{' '}
+//         <Icon
+//           width="150"
+//           height="150"
+//           iconName="icon-arrow-left"
+//           style={iconStyles}
+//         />
+//       </CloseButton>
+//     </ModalWrapper>
+//   );
+// });
+
+// TourModal.displayName = 'TourModal';
+
+// export default TourModal;
