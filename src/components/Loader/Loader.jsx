ded548e37c51f1d29aa710f8435ff9181c5b79e2
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-04-28 19:02:51 +0400

    loadnewpull

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { LoaderContainer, Text, SvgHouse, SvgWave } from './Loader.styled';
+import { LoaderContainer, Text } from './Loader.styled';
+import Iconsvg from '../Icon/Icon';
+
 
 const Loader = () => {
-  return (
+    return (
     <LoaderContainer>
-      <SvgHouse width="31" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M30.276 19.025H.024V9.022L15.151 0l15.125 9.022v10.003z" fill="#F77D07" />
-      </SvgHouse>
+      <Iconsvg  width="38" height="35" iconName="house" styles="house-svg"/>
       <Text>Місто Хаб</Text>
-      <SvgWave width="30" height="15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M36.115 14.107c-4.434 0-6.857-1.63-8.624-2.819-1.296-.872-1.83-1.23-3.414-1.23s-2.122.361-3.414 1.23c-1.768 1.19-4.19 2.819-8.625 2.819-4.434 0-6.857-1.63-8.624-2.821-1.295-.872-1.83-1.231-3.414-1.231V.844c4.434 0 6.857 1.63 8.624 2.821 1.296.872 1.83 1.231 3.414 1.231s2.122-.362 3.414-1.23c1.768-1.19 4.19-2.82 8.625-2.82 4.434 0 6.857 1.63 8.624 2.822 1.295.872 1.83 1.23 3.414 1.23v9.212-.003z" fill="#F77D07" />
-      </SvgWave>
+      <Iconsvg width="40" height="35" iconName="wave" styles="wave-svg"/>
     </LoaderContainer>
   );
 };
-
 export default Loader;
--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -1,5 +1,6 @@
 import styled, { keyframes } from 'styled-components';
 
+
 export const spinner = keyframes`
   to {
     transform: rotate(360deg);
@@ -12,21 +13,26 @@ export const LoaderContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  height: 50vh;
+
+    .house-svg{
+    margin-right: 5px;
+  }
+
+  .wave-svg{
+    margin-left: 1px;
+  }
+  
 `;
 
 export const Text = styled.div`
   text-align: center;
   color: #fff;
+  margin:0;
+  margin-top: -20px;
+  margin-right: 10px;
   -webkit-text-stroke: 0.5px #0B0B0B;
-  font-size: 14px;
+  font-size: 10px;
   animation: ${spinner} 2s linear infinite;
 `;
 
-export const SvgHouse = styled.svg`
-  margin: 0px;
-`;
-
-export const SvgWave = styled.svg`
-  margin: 0;
-`;
