6dd387e3462427e92e60ad1426c80c3acd6bbcf3
Yuriy2223 <yuriy.shukan@gmail.com>
2024-04-28 22:33:10 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,48 +1,46 @@
-// import { useState, useEffect } from 'react';
-// import { Container } from './HomePage.styled';
-// import VirtualTour from '../../components/VirtualTour/VirtualTour';
-// import { fetchGoal, fetchCompanies, fetchPeople } from '../../services/API';
-// import Icon from 'src/components/Icon/Icon';
-// import { ZibraniKoshti } from '../../components/ZibraniKoshti/ZibraniKoshti';
-import Visualization from "../../components/Visualization/Visualization"
+import { useState, useEffect } from 'react';
+import { Container } from './HomePage.styled';
+import VirtualTour from '../../components/VirtualTour/VirtualTour';
+import { fetchGoal, fetchCompanies, fetchPeople } from '../../services/API';
+import Icon from 'src/components/Icon/Icon';
+import { ZibraniKoshti } from '../../components/ZibraniKoshti/ZibraniKoshti';
+import Visualization from '../../components/Visualization/Visualization';
 
 const HomePage = () => {
-  // const [goalData, setGoalData] = useState(null);
-  // const [companiesData, setCompaniesData] = useState(null);
-  // const [peopleData, setPeopleData] = useState(null);
+  const [goalData, setGoalData] = useState(null);
+  const [companiesData, setCompaniesData] = useState(null);
+  const [peopleData, setPeopleData] = useState(null);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const goalResponse = await fetchGoal();
-  //       const companiesResponse = await fetchCompanies();
-  //       const peopleResponse = await fetchPeople();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const goalResponse = await fetchGoal();
+        const companiesResponse = await fetchCompanies();
+        const peopleResponse = await fetchPeople();
 
-  //       setGoalData(goalResponse);
-  //       setCompaniesData(companiesResponse);
-  //       setPeopleData(peopleResponse);
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
+        setGoalData(goalResponse);
+        setCompaniesData(companiesResponse);
+        setPeopleData(peopleResponse);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
-  // function formImgURL(img) {
-  //   const imgData = img.asset._ref.split('-');
+  function formImgURL(img) {
+    const imgData = img.asset._ref.split('-');
 
-  //   return `https://cdn.sanity.io/images/${
-  //     import.meta.env.VITE_ADMIN_PROJECT_ID
-  //   }/${import.meta.env.VITE_ADMIN_DATASET}/${imgData[1]}-${imgData[2]}.${
-  //     imgData[3]
-  //   }`;
-  // }
-  // console.log('done');
+    return `https://cdn.sanity.io/images/${
+      import.meta.env.VITE_ADMIN_PROJECT_ID
+    }/${import.meta.env.VITE_ADMIN_DATASET}/${imgData[1]}-${imgData[2]}.${
+      imgData[3]
+    }`;
+  }
+  console.log('done');
   return (
-    <>
-     {/* <Container> */}
-      {/* <Icon width="40" height="30" iconName="Vector" styles="vector-svg" />
-
+    <Container>
+      <Icon width="40" height="30" iconName="Vector" styles="vector-svg" />
       <h1>Home Page</h1>
       {goalData && (
         <>
@@ -52,10 +50,9 @@ const HomePage = () => {
       {companiesData && <img src={formImgURL(companiesData[0].logoURL)} />}
       {peopleData && <img src={formImgURL(peopleData[0].imageURL)} />}
       <VirtualTour />
-      <ZibraniKoshti /> */}
+      <ZibraniKoshti />
       <Visualization />
-    {/* </Container> */}
-    </>
+    </Container>
   );
 };
 
