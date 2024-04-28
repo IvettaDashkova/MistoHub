50263cde9825d513b1c993ce34d46f69088f76d4
anyalenzy <anya.dudnik.91@gmail.com>
2024-04-29 01:38:03 +0300

    start add teamModal

--- src/components/TeamModal/TeamModal.jsx
+++ src/components/TeamModal/TeamModal.jsx
@@ -1,9 +1,41 @@
 import Modal from 'react-modal';
+import { TeamModalStyled } from './TeamModal.styled';
+import Iconsvg from '../Icon/Icon';
+import data from './develops.json';
 Modal.setAppElement('#root');
 
 const TeamModal = ({ modalTeamIsOpen, modalTeamOnClose }) => {
   return (
-    <Modal isOpen={modalTeamIsOpen} onRequestClose={modalTeamOnClose}></Modal>
+    <TeamModalStyled isOpen={modalTeamIsOpen} onRequestClose={modalTeamOnClose}>
+      <p>Команда</p>
+      <button className="сloseBtn" type="button" onClick={modalTeamOnClose}>
+        <span className="сloseText">Закрити</span>
+        <Iconsvg
+          width="12"
+          height="12"
+          iconName="icon-close-icon"
+          styles="icon-close-icon"
+        />
+      </button>
+      <h2>
+        <span>Команда</span>, що створила сайт
+      </h2>
+      <ul>
+        {data.map((person) => (
+          <li key={person.id}>
+            <Developer
+              name={person.name}
+              role={person.role}
+              link={person.link}
+              jpg={person.jpg}
+              jpgx2={person.jpgx2}
+              webp={person.webp}
+              webpx2={person.webpx2}
+            />
+          </li>
+        ))}
+      </ul>
+    </TeamModalStyled>
   );
 };
 
--- src/components/TeamModal/TeamModal.styled.jsx
+++ src/components/TeamModal/TeamModal.styled.jsx
@@ -1,2 +1,36 @@
 import { styled } from 'styled-components';
+import sprite from '/src/assets/icons.svg';
 import Modal from 'react-modal';
+export const TeamModalStyled = styled(Modal)`
+  background-image: url('${sprite}#icon-bg-modal');
+  background-repeat: no-repeat;
+  font-family: FixelDisplay;
+  background-size: 100% 100%;
+  background-position: 60% 50%;
+  min-width: 320px;
+  max-width: 375px;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 23px 16px 20px 16px;
+  .сloseBtn {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    color: inherit;
+    background: none;
+    border: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+  }
+
+  .icon-close-icon {
+    stroke: rgba(11, 11, 11, 0.5);
+  }
+`;
