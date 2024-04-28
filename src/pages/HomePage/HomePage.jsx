e21a4b5472e19fb0eb05a020d496344e9c64655d
Yuriy2223 <yuriy.shukan@gmail.com>
2024-04-28 23:45:36 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import Modal from 'react-modal';
-// import { ZibraniKoshti } from '../../components/ZibraniKoshti/ZibraniKoshti';
+import { ZibraniKoshti } from '../../components/ZibraniKoshti/ZibraniKoshti';
 import Visualization from '../../components/Visualization/Visualization';
 
 Modal.setAppElement('#root');
@@ -8,8 +8,8 @@ const HomePage = () => {
   return (
     <>
       <main>
-        {/* <ZibraniKoshti /> */}
         <Visualization />
+        <ZibraniKoshti />
       </main>
     </>
   );
