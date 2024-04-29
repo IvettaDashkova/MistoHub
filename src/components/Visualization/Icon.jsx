c1da55650f6876dc226843d4ea29767e7b8fdeea
Yuriy2223 <yuriy.shukan@gmail.com>
2024-04-29 13:00:52 +0300

    Update Icon.jsx

--- src/components/Visualization/Icon.jsx
+++ src/components/Visualization/Icon.jsx
@@ -9,22 +9,3 @@ const Icon = ({ width, height, iconName }) => {
 };
 
 export default Icon;
-
-/**========================== */
-
-//  import styled from 'styled-components';
-
-// const StyledIcon = styled.svg`
-//   fill: ${(props) => props.fill};
-//   stroke: ${(props) => props.stroke};
-// `;
-
-// const Icon = ({ width, height, iconName, fill, stroke }) => {
-//   return (
-//     <StyledIcon width={width} height={height} fill={fill} stroke={stroke}>
-//       <use href={`${sprite}#${iconName}`}></use>
-//     </StyledIcon>
-//   );
-// };
-
-// export default Icon;
