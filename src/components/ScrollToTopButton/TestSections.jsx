6067bda164687c4a6508645aec078bab0d0d7b5d
serg444s <143083342+serg444s@users.noreply.github.com>
2024-04-30 10:15:34 +0300

    test sections
    add test sections for scroll

---
+++ src/components/ScrollToTopButton/TestSections.jsx
@@ -0,0 +1,140 @@
+const TestSections = () => {
+  return (
+    <>
+      <section id="main">
+        <div
+          style={{
+            height: '500px',
+            width: '1200px',
+            padding: '20px',
+            border: '1px solid black',
+            margin: '20px auto',
+          }}
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ipsum,
+            quo illum sint harum odit accusamus maiores placeat fugiat
+            temporibus eos ipsa ea ex eum repellendus nemo saepe corporis
+            dolore!
+          </p>
+        </div>
+      </section>
+      <section id="about">
+        {' '}
+        <div
+          style={{
+            height: '500px',
+            width: '1200px',
+            padding: '20px',
+            border: '1px solid black',
+            margin: '20px auto',
+          }}
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ipsum,
+            quo illum sint harum odit accusamus maiores placeat fugiat
+            temporibus eos ipsa ea ex eum repellendus nemo saepe corporis
+            dolore!
+          </p>
+        </div>
+      </section>
+      <section id="design">
+        {' '}
+        <div
+          style={{
+            height: '500px',
+            width: '1200px',
+            padding: '20px',
+            border: '1px solid black',
+            margin: '20px auto',
+          }}
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ipsum,
+            quo illum sint harum odit accusamus maiores placeat fugiat
+            temporibus eos ipsa ea ex eum repellendus nemo saepe corporis
+            dolore!
+          </p>
+        </div>
+      </section>
+      <section id="funds">
+        {' '}
+        <div
+          style={{
+            height: '500px',
+            width: '1200px',
+            padding: '20px',
+            border: '1px solid black',
+            margin: '20px auto',
+          }}
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ipsum,
+            quo illum sint harum odit accusamus maiores placeat fugiat
+            temporibus eos ipsa ea ex eum repellendus nemo saepe corporis
+            dolore!
+          </p>
+        </div>
+      </section>
+      <section id="investors">
+        {' '}
+        <div
+          style={{
+            height: '500px',
+            width: '1200px',
+            padding: '20px',
+            border: '1px solid black',
+            margin: '20px auto',
+          }}
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ipsum,
+            quo illum sint harum odit accusamus maiores placeat fugiat
+            temporibus eos ipsa ea ex eum repellendus nemo saepe corporis
+            dolore!
+          </p>
+        </div>
+      </section>
+      <section id="how">
+        {' '}
+        <div
+          style={{
+            height: '500px',
+            width: '1200px',
+            padding: '20px',
+            border: '1px solid black',
+            margin: '20px auto',
+          }}
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ipsum,
+            quo illum sint harum odit accusamus maiores placeat fugiat
+            temporibus eos ipsa ea ex eum repellendus nemo saepe corporis
+            dolore!
+          </p>
+        </div>
+      </section>
+      <section id="founders">
+        {' '}
+        <div
+          style={{
+            height: '500px',
+            width: '1200px',
+            padding: '20px',
+            border: '1px solid black',
+            margin: '20px auto',
+          }}
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ipsum,
+            quo illum sint harum odit accusamus maiores placeat fugiat
+            temporibus eos ipsa ea ex eum repellendus nemo saepe corporis
+            dolore!
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TestSections;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import Header from '/src/components/Header/Header';
 import { ZibraniKoshti } from '../../components/ZibraniKoshti/ZibraniKoshti';
 import ScrollToTopButton from '../../components/ScrollToTopButton/ScrollToTopButton';
 import BurgerMenu from '../../components/BurgerMenu/BurgerMenu';
+import TestSections from '../../components/ScrollToTopButton/TestSections';
 
 Modal.setAppElement('#root');
 
@@ -14,6 +15,7 @@ const HomePage = () => {
       <main>
         <ZibraniKoshti />
         <ScrollToTopButton />
+        <TestSections />
       </main>
     </>
   );
