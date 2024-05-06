b49db05cbd42f3dd04fef1c45820b2e76c20c8cf
Mykyta Kudlai <nikiatanik@gmail.com>
2024-05-06 13:33:58 +0200

    deleted useless comments

--- src/components/JoinUs/Background/Background.jsx
+++ src/components/JoinUs/Background/Background.jsx
@@ -1,30 +1,5 @@
 import { BackgroundContainer } from './Background.styled';
 import { images } from '../../../constants/JoinUsBackground';
-// import personCircleJpg from '/src/assets/JoinUs/images/jpg/person-circle.jpg';
-// import personCircleRetinaJpg from '/src/assets/JoinUs/images/jpg/person-circle@2x.jpg';
-// import personTriangleJpg from '/src/assets/JoinUs/images/jpg/person-triangle.png';
-// import personTriangleRetinaJpg from '/src/assets/JoinUs/images/jpg/person-triangle@2x.png';
-// import personStarJpg from '/src/assets/JoinUs/images/jpg/person-star.png';
-// import personStarRetinaJpg from '/src/assets/JoinUs/images/jpg/person-star@2x.png';
-// import cafeJpg from '/src/assets/JoinUs/images/jpg/cafe.jpg';
-// import cafeRetinaJpg from '/src/assets/JoinUs/images/jpg/cafe@2x.jpg';
-// import cafeOvalJpg from '/src/assets/JoinUs/images/jpg/cafe-oval.png';
-// import cafeOvalRetinaJpg from '/src/assets/JoinUs/images/jpg/cafe-oval@2x.png';
-// import cafeSquareJpg from '/src/assets/JoinUs/images/jpg/cafe-square.png';
-// import cafeSquareRetinaJpg from '/src/assets/JoinUs/images/jpg/cafe-square@2x.png';
-
-// import personCircleWebP from '/src/assets/JoinUs/images/webp/person-circle.webp';
-// import personCircleRetinaWebP from '/src/assets/JoinUs/images/webp/person-circle@2x.webp';
-// import personTriangleWebP from '/src/assets/JoinUs/images/webp/person-triangle.webp';
-// import personTriangleRetinaWebP from '/src/assets/JoinUs/images/webp/person-triangle@2x.webp';
-// import personStarWebP from '/src/assets/JoinUs/images/webp/person-star.webp';
-// import personStarRetinaWebP from '/src/assets/JoinUs/images/webp/person-star@2x.webp';
-// import cafeWebP from '/src/assets/JoinUs/images/webp/cafe.webp';
-// import cafeRetinaWebP from '/src/assets/JoinUs/images/webp/cafe@2x.webp';
-// import cafeOvalWebP from '/src/assets/JoinUs/images/webp/cafe-oval.webp';
-// import cafeOvalRetinaWebP from '/src/assets/JoinUs/images/webp/cafe-oval@2x.webp';
-// import cafeSquareWebP from '/src/assets/JoinUs/images/webp/cafe-square.webp';
-// import cafeSquareRetinaWebP from '/src/assets/JoinUs/images/webp/cafe-square@2x.webp';
 
 const Background = () => {
   return (
