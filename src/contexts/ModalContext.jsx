8ac9b4a2a8e857cbb99c4354e67d081b39f6d714
SerhiiMonzhelesov <129993794+SerhiiMonzhelesov@users.noreply.github.com>
2024-05-06 14:45:10 +0300

    fixed path to img modal team

--- src/contexts/ModalContext.jsx
+++ src/contexts/ModalContext.jsx
@@ -8,7 +8,6 @@ export const ModalProvider = ({ children }) => {
     menu_modal: false,
     team_modal: false,
   });
-  console.log('isModalOpen: ', isModalOpen);
 
   const openModal = (modalName) => {
     setIsModalOpen((prevState) => ({ ...prevState, [modalName]: true }));
