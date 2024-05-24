353338f9b502cdad4bf0c8740431cf07ed834600
SerhiiMonzhelesov <129993794+SerhiiMonzhelesov@users.noreply.github.com>
2024-05-24 11:54:28 +0300

    fix path`s for build

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import GlobalStyle from './globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/MistoHub">
+  <BrowserRouter basename="/">
     <GlobalStyle />
     <App />
   </BrowserRouter>
