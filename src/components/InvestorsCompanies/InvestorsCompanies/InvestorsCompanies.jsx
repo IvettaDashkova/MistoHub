15cfca49245b39b0da511b195e21e6edd572ec8a
SerhiiMonzhelesov <129993794+SerhiiMonzhelesov@users.noreply.github.com>
2024-05-29 20:56:01 +0300

    fix companies animation

--- src/components/InvestorsCompanies/InvestorsCompanies/InvestorsCompanies.jsx
+++ src/components/InvestorsCompanies/InvestorsCompanies/InvestorsCompanies.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import ModalCompanies from '../ModalCompanies/ModalCompanies';
-import { CompanyWrapper } from './InvestorsCompanies.styled';
+import { CompaniesList, CompanyWrapper } from './InvestorsCompanies.styled';
 import { nanoid } from 'nanoid';
 import defaultImage from 'src/assets/investors/default-img.png';
+import { makerAnimParams } from '../../../helpers/makerAnimParams';
+import { useMediaQuery } from 'react-responsive';
 
 const InvestorsCompanies = ({ companies: companiesData }) => {
   const [selectedCompanies, setSelectedCompanies] = useState(null);
+  const isDesktop = useMediaQuery({ minWidth: 1440 });
+  const isTablet = useMediaQuery({ minWidth: 768 });
 
   const openModal = (company) => {
     setSelectedCompanies(company);
@@ -20,27 +24,27 @@ const InvestorsCompanies = ({ companies: companiesData }) => {
   return (
     <CompanyWrapper>
       {Array.isArray(companiesData) && (
-        <div>
-          <ul>
-            {companiesData.map((company) => (
-              <li key={nanoid()}>
-                <img
-                  src={company.logoURL || defaultImage}
-                  alt={`Company ${company.id}`}
-                  onClick={() => openModal(company)}
-                  loading="lazy"
-                />
-              </li>
-            ))}
-            {selectedCompanies && (
-              <ModalCompanies
-                isOpen={selectedCompanies !== null}
-                data={selectedCompanies}
-                onClose={closeModal}
+        <CompaniesList
+          $anim={makerAnimParams(companiesData.length, isDesktop, isTablet)}
+        >
+          {Array.from([...companiesData, ...companiesData]).map((company) => (
+            <li key={nanoid()}>
+              <img
+                src={company.logoURL || defaultImage}
+                alt={`Company ${company.id}`}
+                onClick={() => openModal(company)}
+                loading="lazy"
               />
-            )}
-          </ul>
-        </div>
+            </li>
+          ))}
+          {selectedCompanies && (
+            <ModalCompanies
+              isOpen={selectedCompanies !== null}
+              data={selectedCompanies}
+              onClose={closeModal}
+            />
+          )}
+        </CompaniesList>
       )}
     </CompanyWrapper>
   );
